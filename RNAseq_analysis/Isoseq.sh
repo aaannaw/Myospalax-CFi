68abaffe981d6f7d3debe1d071d292f31b22c7a4
aaannaw <109299053+aaannaw@users.noreply.github.com>
2025-02-20 16:38:59 +0800

    Create Isoseq.sh

---
+++ RNAseq_analysis/Isoseq.sh
@@ -0,0 +1,2 @@
+/data/01/user157/software/nextflow run /data/01/user157/software/wf-transcriptomes --fastq ./Ma.isoseq.fastq.gz --minimap2_index_opts '-k15' --direct_rna --ref_genome Ma6.final.chr.fa 
+/data/01/user157/software/nextflow run /data/01/user157/software/wf-transcriptomes --fastq ./Mp.isoseq.fastq.gz --minimap2_index_opts '-k15' --direct_rna --ref_genome Mp5.final.chr.fa 
