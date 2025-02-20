641dd55cefdd1d6e55e61827c3bb539578ae4afc
aaannaw <109299053+aaannaw@users.noreply.github.com>
2025-02-20 10:42:19 +0800

    Update HiC-pro.sh

--- HiC_analysis/HiC-pro.sh
+++ HiC_analysis/HiC-pro.sh
@@ -2,5 +2,9 @@
 python3 HiC-Pro_3.1.0/bin/utils/split_reads.py --results_folder  .  --nreads 20000000 Ma6_R1.fastq
 python3 HiC-Pro_3.1.0/bin/utils/split_reads.py --results_folder  .  --nreads 20000000 Ma6_R2.fastq
 python3 /data/01/user157/software/hic-pro/HiC-Pro_3.1.0/bin/utils/digest_genome.py -r ^GATC -o Ma6.MBOI.bed  Ma6.final.Chr.fasta
-
+bowtie2-build --threads 20 Ma6.Chr.2210.fasta Ma
+/data/01/user157/software/Hic-Pro/hic-pro-install/HiC-Pro-3.1.0/bin/HiC-Pro -i fastq -o results -c config-hicpro.txt
 #FitHiC
+/data/01/user157/software/Hic-Pro/hic-pro-install/HiC-Pro-3.1.0/bin/utils/hicpro2fithic.py -i chroder_20000_iced.matrix -b chroder_20000_abs.bed -s  chroder_20000_iced.matrix.biases
+fithic -f fithic.fragmentMappability.gz -i fithic.interactionCounts.gz -t fithic.biases.gz -o fithic -l Ma -v -x All -r 20000
+python runfilterpvalue.qvalue.count.py Ma.spline_pass1.res20000.significances.txt.gz Ma.spline_pass1.res20000.significances.txt.bed
